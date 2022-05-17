8058e83f1e6171f070b85864d46d8e3e9672059b
numbergazing <manoahbernier@gmail.com>
2022-05-17 22:35:22 +0200

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='TheCoverProject',
+    version='0.1.0',
+    packages=['thecoverproject'],
+    url='https://github.com/numbergazing/TheCoverProject',
+    license='MIT',
+    author='numbergazing',
+    author_email='manoahbernier@gmail.com',
+    description='A Python library to download game covers from http://thecoverproject.net/'
+)
